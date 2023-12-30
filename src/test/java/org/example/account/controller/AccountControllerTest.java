8b752d624237386c2fcf50d06fa8877a3738c547
(GangPil_Song) <songgangpil@gmail.com>
2023-12-30 16:30:56 +0900

    file commit.

--- src/test/java/org/example/account/controller/AccountControllerTest.java
+++ src/test/java/org/example/account/controller/AccountControllerTest.java
@@ -9,13 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AccountController.class)
--- src/test/java/org/example/account/service/AccountServiceTest.java
+++ src/test/java/org/example/account/service/AccountServiceTest.java
@@ -6,15 +6,19 @@ import org.example.account.repository.AccountRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class AccountServiceTest {
@@ -25,7 +29,7 @@ class AccountServiceTest {
     private AccountService accountService;
 
     @Test
-    @DisplayName("계좌 조회 성공")
+    @DisplayName("test 이름 변경")
     void testXXX() {
         //given
         given(accountRepository.findById(anyLong()))
@@ -34,7 +38,7 @@ class AccountServiceTest {
                         .accountNumber("69780")
                         .build()));
         //when
-        Account account = accountService.getAccount(2555L);
+        Account account = accountService.getAccount(4555L);
 
         //then
         assertEquals("69780", account.getAccountNumber());
@@ -42,21 +46,39 @@ class AccountServiceTest {
     }
 
     @Test
-    @DisplayName("Test 이름 변경")
+    @DisplayName("계좌 조회 성공")
     void testGetAccount1() {
         //given
         given(accountRepository.findById(anyLong()))
                 .willReturn(Optional.of(Account.builder()
                         .accountStatus(AccountStatus.UNREGISTERED)
-                        .accountNumber("69780")
-                        .build()));
+                        .accountNumber("69780").build()));
+        ArgumentCaptor<Long> captor = ArgumentCaptor.forClass(Long.class);
         //when
-        Account account = accountService.getAccount(2555L);
+        Account account = accountService.getAccount(4555L);
 
         //then
+        verify(accountRepository, times(1)).findById(captor.capture());
+        verify(accountRepository, times(0)).save(any());
+        assertEquals(4555L, captor.getValue());
+        assertNotEquals(45515L, captor.getValue());
         assertEquals("69780", account.getAccountNumber());
         assertEquals(AccountStatus.UNREGISTERED, account.getAccountStatus());
     }
+
+    @Test
+    @DisplayName("계좌 조회 실패 - 음수로 조회")
+    void testFailedToSearchAccount() {
+        //given
+        //when
+        RuntimeException runtimeException = assertThrows(RuntimeException.class,
+                () -> accountService.getAccount(-10L));
+
+        //then
+        assertEquals("Minus", runtimeException.getMessage());
+    }
+
+
 
     @Test
     void testGetAccount2() {
@@ -67,7 +89,7 @@ class AccountServiceTest {
                         .accountNumber("69780")
                         .build()));
         //when
-        Account account = accountService.getAccount(2555L);
+        Account account = accountService.getAccount(4555L);
 
         //then
         assertEquals("69780", account.getAccountNumber());
